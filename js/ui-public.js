6183f2cfe83c1e28713c390b50120fb693ac9aab
Matt Alice <matthias.alice@gmail.com>
2014-11-09 21:14:34 +0000

    First commit, adding html, css, js and images files

---
+++ js/ui-public.js
@@ -0,0 +1,52 @@
+// Packing-slips.com Notification widget
+// Copyright packing-slips.com 2009
+
+/*
+<doc>
+    Dependencies
+        jQuery
+        /public_html/lib/ui/css/ui.css
+        /public_html/lib/ui/images/*
+   
+    Initialization:
+        $('#uniqueSelectorOnPage').displayMsg({
+            msgType  : 'success',
+            msgTitle : 'Heads up!!' ,
+            ...
+        })
+
+    Params: you can provide the following options
+        @msgType 
+            Select one the 3 following options: success, warning or error
+            If none is provided, or if the one provided upon initialization is not n this array, the msgType will default to "success"
+        @msgTitle
+            String of text or html to put in title of the displayMsg widget
+        @msgContent
+            String of text or html to put in contents of the displayMsg widget
+         ?? @msgContentUrl -- coming soon ??   
+            Location of remote AJAX displayMsg widget content
+</doc>        
+*/
+
+(function($) {
+    $.fn.displayMsg = function(options) {
+    
+        var custom = $.extend({
+                msgType    : 'success',
+                msgTitle   : 'Success',
+                msgContent : 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vivamus dignissim euismod magna. Maecenas eu pede. Mauris posuere. Vivamus mi'
+            }, options);
+        
+        if (!$.inArray(custom.msgType, ['success','error','warning'])) {
+            custom.msgType = 'success';
+        }
+
+        this.html('<div class="message-box message-box-wrapper-' + custom.msgType + '"> \
+                    <div class="message-box-logo-' + custom.msgType + '"></div> \
+                    <div class="message-box-inner-' + custom.msgType + '"> \
+                        <b>'  + custom.msgTitle   + '</b> \
+                        <br>' + custom.msgContent + '  \
+                    </div> \
+                </div>');
+    }   
+})(jQuery);
